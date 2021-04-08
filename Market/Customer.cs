0d3d1114c628d25d1c59e4f5c89f9ff8d1d18c2c
Bohdan T <bohdan013@gmail.com>
2021-04-08 19:28:48 +0300

    More logic implemented

--- Market/Customer.cs
+++ Market/Customer.cs
@@ -8,7 +8,12 @@ namespace Market
 {
     class Customer
     {
+
+        // Field
         private Balance balance;
+        private Cart cart;
+
+        // Property
         public Balance Balance
         {
             get { return balance; }
@@ -17,20 +22,22 @@ namespace Market
         // Constructor
         public Customer()
         {
-             balance = new Balance();
-
+            balance = new Balance();
+            cart = new Cart();
         }
 
+        // Method.
         public void Purchase()
         {
 
         }
 
-        public void SelectGood(string name, int quantity, Storage storage)
+        // Method.
+        public void SelectGood(Good good, Storage storage, int quantity)
         {
-            //storage.goods
 
-
+            storage.TakeGoodFromStorage(good, storage, quantity);
+            cart.AddGood(good);
         }
 
     }
--- Market/Program.cs
+++ Market/Program.cs
@@ -7,14 +7,23 @@ namespace Market
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Welcome to the Market!\n\nSelect one of the options below:\n");
+            Console.WriteLine("\t1.Enter Marker.\n\t2.Display available goods.\n\t3.Add good to the cart.\n\t4.Remove good from the cart.");
+            Console.WriteLine("\t5.Purchase added to cart goods.\n\t6.Leave market");
             Customer customer = new Customer();
 
             Storage storage = new Storage();
 
-            storage.GetGoods();
+            storage.DisplayGoods();
 
-            Console.WriteLine(customer.Balance.Balance_card);
+
+
+
+
+            //Console.WriteLine(customer.Balance.Balance_card);
             Console.ReadKey();
+
         }
     }
 }
