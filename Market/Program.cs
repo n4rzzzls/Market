5b5bf18932a0306f611989ab04a8f13cdc4f8258
Bohdan T <bohdan013@gmail.com>
2021-04-09 20:50:08 +0300

    Logic for Program.cs

--- Market/Program.cs
+++ Market/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Text;
 
 namespace Market
 {
@@ -8,14 +9,67 @@ namespace Market
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to the Market!\n\nSelect one of the options below:\n");
-            Console.WriteLine("\t1.Enter Marker.\n\t2.Display available goods.\n\t3.Add good to the cart.\n\t4.Remove good from the cart.");
-            Console.WriteLine("\t5.Purchase added to cart goods.\n\t6.Leave market");
-            Customer customer = new Customer();
-
+            // Variables
+            object pressed_key;
+            Customer customer;
             Storage storage = new Storage();
 
-            storage.DisplayGoods();
+
+            Console.WriteLine("1.Enter Marker.");
+            pressed_key = Console.ReadKey();
+            // TO DO
+
+            customer = new Customer();
+
+            // Some console output
+            Console.WriteLine("Welcome to the Market!\n\nSelect one of the options below:\n");
+            Console.WriteLine("\t1.Display available goods.\n\t2.Add good to the cart.\n\t3.Remove good from the cart.");
+            Console.WriteLine("\t4.Purchase added to cart goods.\n\t5.Leave market");
+
+
+            // User input. Verifies that entered variable is int type.
+            while (true)
+            {
+                pressed_key = Console.Read();
+                if (pressed_key.GetType() == typeof(int))
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("A number should be selected.");
+                }
+            }
+
+
+            // Switch/case
+            switch (pressed_key)
+            {
+                case 1:
+                    storage.DisplayGoods();
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Select proper good to be added into cart.\nEnter ID of good: ");
+                    int selected_good = Console.Read();
+
+                    //customer.SelectGood()
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+            }
+
+
+            //storage.DisplayGoods();
 
 
 
